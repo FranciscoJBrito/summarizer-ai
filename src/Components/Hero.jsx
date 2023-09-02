0606d033f8c3e4b3dd4acd0756afe2a822441713
Francisco <franciscojavierbrito16@gmail.com>
2023-09-02 13:35:10 -0300

    #2

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,12 +6,13 @@ const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-        <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
+        {/* <img src={logo} alt='sumz_logo' className='w-36 object-contain mt-1' /> */}
+        <h2 className="text-green-600 text-3xl font-bold">Summe<span className="text-lime-400">.</span></h2>
 
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/FranciscoJBrito/summarizer-ai", "_blank")
           }
           className='black_btn'
         >
